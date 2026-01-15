62950ff5502eb3a965c5ae12f1d1760b82e2a0b4
gollilla <soradore2@gmail.com>
2026-01-15 19:40:20 +0900

    Update vite.config.app.js

--- vite.config.app.js
+++ vite.config.app.js
@@ -3,10 +3,16 @@ import {
   behaviorPacker as keystonePlugin,
   supportJsx,
 } from 'keystonemc/vite-plugin';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [
     keystonePlugin({ uuid: "82b9174d-ea3d-42ae-986c-b42ce3644760" }),
     supportJsx(),
   ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
 });
