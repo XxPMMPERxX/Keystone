e5cf15aebfc85eac84e117f2e3eb3c94298c9121
gollilla <soradore2@gmail.com>
2026-01-18 03:07:49 +0900

    update: keystonemc v4.2.0

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
   ],
   rules: {
     'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
-    'semi': ['error', 'always']
+    'semi': ['error', 'always'],
+    'jsx-quotes': ['error', 'prefer-double'],
   },
 });
